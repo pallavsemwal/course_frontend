54f1f4d9b50fc7ba4399b1fa50cb2845858a3e41
pallavsemwal <pallav.semwal24@gmail.com>
2023-03-31 13:24:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container homepage'>
+      <h3>
+      Hi Welcome to the Assesment!!
+      </h3>
+      <p>
+        Bootstrap + React-Bootstrap Has Already Been Installed And You Can Start Coding!!
+      </p>
+      <ul>
+
+      </ul>
     </div>
   );
 }
